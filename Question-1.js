4a831e45329d4f7d4fedd60c7b355772cd38d01b
Faiyaz369 <mohtasimfaiyaz369@gmail.com>
2026-09-02 21:06:02 +0600

    Create Question-1.js

---
+++ Question-1.js
@@ -0,0 +1,9 @@
+//Question-1
+
+function describeValue(value){
+    let val_type = typeof(value);//taking the type of value
+    let tr_val;
+    (value)? tr_val = "Truthy" : tr_val = "Falsy";
+    return val_type + " | " + tr_val; 
+}
+console.log(describeValue("0"));
